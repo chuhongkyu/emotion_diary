98c64934081d28a98a8a4bef641034ac0c95b78b
Mr.chu <chuhongkyu@gmail.com>
2022-04-13 09:29:39 +0900

    Initial commit

---
+++ src/pages/Edit.js
@@ -0,0 +1,10 @@
+const Edit = ()=>{
+    return(
+        <div>
+        <h1>Edit</h1>
+        <p>이곳은 수정페이지입니다</p>
+        </div>
+    );
+}
+
+export default Edit;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = ()=>{
+    return(
+        <div>
+        <h1>Home</h1>
+        <p>여기는 홈입니다.</p>
+        </div>
+    );
+}
+
+export default Home;
