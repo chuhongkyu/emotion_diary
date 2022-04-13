7aab7ba6ed3c12e1e1e04b903c1da858d687e9f4
Mr.chu <chuhongkyu@gmail.com>
2022-04-13 09:49:07 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import RouteTest from './components/RouteTest';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+        <div className="App">
+            <h2>App.js</h2>
+            <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/new' element={<New/>} />
+              <Route path='/edit' element={<Edit/>} />
+              <Route path='/diary' element={<Diary/>} />
+            </Routes>
+            <RouteTest/>
+        </div>
+    </BrowserRouter>
   );
 }
 
