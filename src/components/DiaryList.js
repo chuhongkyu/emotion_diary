a22bc5d0d40ee6c13bd838de18e21015a997709e
Mr.chu <chuhongkyu@gmail.com>
2022-04-18 16:18:54 +0900

    Update_DairyList

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
+
+const sortOptionList =[
+    {value:"latest", name:"최신순"},
+    {value:"older", name:"오래된 순"},
+]
+
 const ControlMenu = ({value, onChange, optionList})=>{
-    return <select></select>
+    return (
+        <select value={value} onChange={(e)=> onChange(e.target.value)}>
+            {optionList.map((it,idx)=><option key={idx} value={it.value}>{it.name}</option>)}
+        </select>
+    )
+            
 }
 
 
 const DiaryList = ({diaryList})=>{
+
+    const[sortType, setSortType] = useState("lastest");
+
     return(
     <div>
-        <ControlMenu/>
+        <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
         {diaryList.map((it)=> (
             <div key={it.id}>{it.content}</div>
             ))}
